bd052bd619c428eb606e654c63e9dd4d55529fac
Akashyadav18 <shadowlee5469@gmail.com>
2023-09-16 18:36:01 +0530

    navbar

---
+++ src/components/authLinks/AuthLinks.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './authLinks.module.css'
+
+
+const AuthLinks = () => {
+  return (
+    <div className={styles.container}>
+      login
+    </div>
+  )
+}
+
+export default AuthLinks
