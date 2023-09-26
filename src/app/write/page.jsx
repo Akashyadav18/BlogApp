1c8ba46cde1a40f0576dc6ea6af286a604089324
Akashyadav18 <shadowlee5469@gmail.com>
2023-09-26 20:39:44 +0530

    posts

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -5,11 +5,24 @@ import styles from "./write.module.css"
 import Image from 'next/image'
 import ReactQuill from 'react-quill'
 import "react-quill/dist/quill.bubble.css"
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 
 const Write = () => {
 
+    const {status} = useSession();
+    const router = useRouter();
+
     const [open, setOpen] = useState(false);
     const [value, setValue] = useState("");
+
+    if(status === "loading"){
+        return <p>Loading...</p>
+    }
+
+    if(status === "authenticated"){
+        router.push("/");
+    }
 
     return (
         <div className={styles.container}>
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,22 +3,24 @@ import styles from "./card.module.css"
 import Image from 'next/image'
 import Link from 'next/link'
 
-const Card = () => {
+const Card = ({ key, item }) => {
     return (
-        <div className={styles.container}>
-            <div className={styles.imageContainer}>
-                <Image src="/p1.jpeg" alt='p1' fill className={styles.image}/>
-            </div>
+        <div className={styles.container} key={key}>
+            {item.img && (
+                <div className={styles.imageContainer}>
+                    <Image src={item.img} alt='p1' fill className={styles.image} />
+                </div>
+            )}
             <div className={styles.textContainer}>
                 <div className={styles.detail}>
-                    <span className={styles.date}>11.02.2023 - </span>
-                    <span className={styles.category}>CULTURE</span>
+                    <span className={styles.date}>{item.createdAt.substring(0, 10)} -{" "}</span>
+                    <span className={styles.category}>{item.catSlug}</span>
                 </div>
-                <Link href="/">
-                    <h1>Lorem ipsum dolor sit amet alism</h1>
+                <Link href={`/posts/${item.slug}`}>
+                    <h1>{item.title}</h1>
                 </Link>
-                <p className={styles.desc}>Jujutsu Kaisen is a Japanese manga series written and illustrated by Gege Akutami. It has been serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump since March 2018, with its chapters collected and published in 23 tankōbon volumes as of July 2023.</p>
-                <Link href="/" className={styles.link}>Read More</Link>
+                <p className={styles.desc}>{item.desc.substring(0, 50)}</p>
+                <Link href={`/posts/${item.slug}`} className={styles.link}>Read More</Link>
             </div>
 
         </div>
